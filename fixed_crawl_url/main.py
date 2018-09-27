ab93b422cd06567384f677f1411d4f6c2eb2e3cf
vu <mainguyenvu210@gmail.com>
2018-09-27 16:36:26 +0700

    ready to auto crawl again

--- fixed_crawl_url/main.py
+++ fixed_crawl_url/main.py
@@ -6,16 +6,16 @@ from matching_products import matchingProduct
 
 def main():
     PROJECT_NAME = "URL"
-    # tikiSpider = Spider(PROJECT_NAME, "tiki")
-    # adayroiSpider = Spider(PROJECT_NAME, "adayroi")
-    # cellphonesSpider = Spider(PROJECT_NAME, "cellphones")
-    #
-    # create_compare_list_of_phones(PROJECT_NAME)
-    #
-    # tiki_matching = matchingProduct(PROJECT_NAME, 'tiki')
-    # tiki_matching.matching()
-    adayroi_matching = matchingProduct(PROJECT_NAME, 'adayroi')
-    adayroi_matching.matching()
+    tikiSpider = Spider(PROJECT_NAME, "tiki")
+    adayroiSpider = Spider(PROJECT_NAME, "adayroi")
+    cellphonesSpider = Spider(PROJECT_NAME, "cellphones")
+    
+    create_compare_list_of_phones(PROJECT_NAME)
+    
+    tiki_matching = matchingProduct(PROJECT_NAME, 'tiki')
+    tiki_matching.matching()
+    # adayroi_matching = matchingProduct(PROJECT_NAME, 'adayroi')
+    # adayroi_matching.matching()
     cellphones_matching = matchingProduct(PROJECT_NAME, 'cellphones')
     cellphones_matching.matching()
 
